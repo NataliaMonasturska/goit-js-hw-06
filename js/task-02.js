8cdc1a8eaf387c0950187109426fc344ed5b017b
Natalia29011989 <tjomamon@gmail.com>
2022-05-07 23:04:45 +0300

    2задача

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const UlIngredients = document.querySelector("ul#ingredients");
+
+  const ingredientsList = ingredients.map((elem) => {
+    const MakeLi = document.createElement("li");
+    MakeLi.textContent = elem;
+    MakeLi.classList.add("item")
+    return MakeLi;
+  }); 
+  UlIngredients.append(...ingredientsList);
