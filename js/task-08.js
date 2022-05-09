73ef125b90631765ba224e0b65583dcf2a4d467f
Natalia29011989 <tjomamon@gmail.com>
2022-05-09 15:00:12 +0300

    8задача

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const Form = document.querySelector("form.login-form");
+
+const ValidationForm = (event) => {
+    event.preventDefault();
+    const Email = event.currentTarget.elements.email;
+    const Password = event.currentTarget.elements.password;
+
+    if (Email.value === ""||Password.value === "") {
+        alert("Все поля должны быть заполнены!")
+       
+    }
+    else{const Values = {
+        [Email.name]: Email.value,
+        [Password.name]: Password.value
+    }
+    console.log(Values)
+    Form.reset();}
+  
+}
+
+Form.addEventListener("submit", ValidationForm )
 
