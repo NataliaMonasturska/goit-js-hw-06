8e7ff9035c5ca198dfcf4f3d5e29052169a2d617
Natalia29011989 <tjomamon@gmail.com>
2022-05-09 15:45:02 +0300

    9задача

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,25 @@
+const Button = document.querySelector("button.change-color")
+const Body = document.querySelector("body")
+const SpanColor = Body.querySelector("span.color")
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const MakeBodyColor = (event) => {
+  Body.style.backgroundColor = getRandomHexColor();
+  SpanColor.textContent = Body.style.backgroundColor;
+}
+Button.addEventListener("click", MakeBodyColor)
+  
+
+
+
+
+
+
+
+
+
+
+
