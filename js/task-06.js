d3b39b63cdc759e330ad13b4737c77bcc1e9b8b2
Natalia29011989 <tjomamon@gmail.com>
2022-05-10 16:13:15 +0300

    7 пофиксила

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,6 @@
 const Input = document.querySelector("input#validation-input");
 const DataInput = document.querySelector('input[data-length="6"]');
 
-console.log(Input);
 const validation = (event) => {
     if (event.currentTarget.value.length < Number(DataInput.dataset.length)) {
       Input.classList.add("invalid") 
--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,9 @@
 const InputFontSizeControl = document.querySelector("input#font-size-control");
 const SpanText = document.querySelector('span#text');
-
+SpanText.style.fontSize = `${InputFontSizeControl.value}px`
 const NewFontSize = () => {
 SpanText.style.fontSize = `${InputFontSizeControl.value}px`
 }
 
 InputFontSizeControl.addEventListener("input", NewFontSize )
+console.log(InputFontSizeControl.value);
